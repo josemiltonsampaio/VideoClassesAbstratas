8f32f1c908e6a7c038191b02ead1b5d505682628
José Milton Sampaio <josemiltondev@gmail.com>
2022-06-23 16:46:29 -0300

    Add project files.

---
+++ VideoClassesAbstratas/Program.cs
@@ -0,0 +1,35 @@
+﻿using VideoClassesAbstratas;
+
+var moto1 = new Moto();
+var carro1 = new Carro();
+
+moto1.Ligar();
+
+Console.WriteLine("Limpar moto1");
+LimparVeiculo(moto1);
+
+Console.WriteLine("Limpar carro1");
+LimparVeiculo(carro1);
+
+Console.WriteLine("Buzinar moto1");
+ApertarBuzina(moto1);
+
+Console.WriteLine("Buzinar carro1");
+ApertarBuzina(carro1);
+
+void LimparVeiculo(Veiculo veiculo)
+{
+    if (veiculo.Ligado)
+    {
+        Console.WriteLine("Por favor desligue o veículo");
+    }
+    else
+    {
+        Console.WriteLine("Iniciando limpeza");
+    }
+}
+
+void ApertarBuzina(Veiculo veiculo)
+{
+    Console.WriteLine($"Veículo buzinando: {veiculo.Buzinar()}");
+}
---
+++ VideoClassesAbstratas/Veiculo.cs
@@ -0,0 +1,48 @@
+﻿namespace VideoClassesAbstratas;
+public abstract class Veiculo
+{
+    public int NivelCombustivel { get; set; }
+    public bool Ligado { get; set; }
+    public void Ligar() => Ligado = true;
+    public void Desligar() => Ligado = false;
+    public virtual string Buzinar() => "FOM-FOM";
+    public bool EstaLigado() => Ligado;
+
+}
+
+public class Moto : Veiculo
+{
+    public override string Buzinar()
+    {
+        return "BI-BI";
+    }
+}
+
+public class Carro : Veiculo
+{
+
+}
+
+//public abstract class Veiculo
+//{
+//    public int NivelCombustivel { get; set; }
+//    public bool Ligado { get; set; }
+//    public abstract void Ligar();
+//    public abstract void Desligar();
+//    public abstract string Buzinar();
+//    public bool EstaLigado() => Ligado;
+
+//}
+//public class Moto : Veiculo
+//{
+//    public override string Buzinar() => "BI-BI";
+//    public override void Desligar() => Ligado = false;
+//    public override void Ligar() => Ligado = true;
+//}
+
+//public class Carro : Veiculo
+//{
+//    public override string Buzinar() => "FOM-FOM";
+//    public override void Desligar() => Ligado = false;
+//    public override void Ligar() => Ligado = true;
+//}
